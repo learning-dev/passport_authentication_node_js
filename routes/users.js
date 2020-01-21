e88132e2d764de9cae440f256434fce8b946edd8
learning-dev <ayazm.140895@gmail.com>
2020-01-21 11:49:57 +0530

    add views for login, register and weclome

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 
 // Login page
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('login'));
 
 // Register page
-router.get('/register', (req, res) => res.send('Login'));
+router.get('/register', (req, res) => res.render('register'));
 
 module.exports = router;
 
