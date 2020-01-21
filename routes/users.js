18e75449094ab46bd215097966c39ec000bb1dd7
learning-dev <ayazm.140895@gmail.com>
2020-01-21 18:24:05 +0530

    add users model to database i.e register functionality

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 
+//user Model
+const User = require('../models/User');
 
 // Login page
 router.get('/login', (req, res) => res.render('login'));
@@ -10,13 +13,14 @@ router.get('/login', (req, res) => res.render('login'));
 router.get('/register', (req, res) => res.render('register'));
 
 // Register Handle
+
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body;
   const errors = [];
 
 
   // check required fields
-  if (!name || !email || !password || password2) {
+  if (!name || !email || !password || !password2) {
     errors.push({ msg: 'Please fill in the details' });
   }
 
@@ -39,10 +43,42 @@ router.post('/register', (req, res) => {
       password2
     });
   } else {
-    res.send('pass')
+    //Validation Passed
+    User.findOne({ email: email})
+    .then((user) => {
+      if (user) {
+      errors.push({msg: 'User email is already registered' })
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+      });
+      } else {
+        const newUser = new User({
+          name, 
+          email, 
+          password
+        })
+        // hashed password
+        bcrypt.genSalt(10, (err, salt) =>{
+            bcrypt.hash(newUser.password, salt, (err, hash) =>{
+              if (err) throw err;
+              // Set password to hashed password
+              newUser.password = hash;
+              newUser.save()
+                .then( user =>{
+                  res.redirect('/users/login');
+                })
+                .catch(err => console.log(err));
+            })
+        } )
+      }
+    });
+    
   }
 
 });
 
 module.exports = router;
-
